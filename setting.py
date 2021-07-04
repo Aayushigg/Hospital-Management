cbb959e44784f77191c793cd9f0c2e48d492f24b
Aayushigg <86644996+Aayushigg@users.noreply.github.com>
2021-07-04 13:08:49 +0530

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,1 +1,5 @@
-# Hospital-Management
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_en)]
+VENV_PATH = os.path.dirname(BASE_DIR) 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root') 
+MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root') 
+AUTH_USER_MODEL = 'account.User'
